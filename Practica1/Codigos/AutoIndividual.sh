1ddf9b3fdfbeedfb7bc699e3b6373aab62119b19
Joaquín Avilés de la Fuente <joaquinrojo724@gmail.com>
2024-03-06 21:12:22 +0100

    Cambios individual

--- Practica1/Codigos/AutoIndividual.sh
+++ Practica1/Codigos/AutoIndividual.sh
@@ -2,23 +2,24 @@
 # Este archivo es como el autorun pero sirve para ejecutar un solo archivo, en este
 # caso, el archivo especificado en la siguiente variable:
 
-name="quicksort"
+name="floyd"
 rm -r Grafica$name
 
 # Crear un directorio para guardar las gráficas
 mkdir Grafica$name
 
 for ((j=1; j<=3 ; j++)) do
-    for ((i=50000; i<=1250000 ; i+=50000)) do
+    for ((i=50; i<=200 ; i+=50)) do
             printf "$i \t" >> Grafica$name/SalidaTest$j$name.txt
             ./Ejecutables/$name $i $j >> Grafica$name/SalidaTest$j$name.txt            
             printf "\n" >> Grafica$name/SalidaTest$j$name.txt
     done
 done
 
-# Crear un script de Gnuplot para generar la gráfica
+# Crear un script de Gnuplot para generar la gráfica, todo lo que hay entre EOF y EOF se escribe en un archivo,
+# en este caso, script_gnuplot.gp
 cat << EOF > script_gnuplot.gp
-# Configuración del gráfico
+# Configuración del gráfico, se define el nombre de los ejes
 set xlabel "Tamaño de entrada"
 set ylabel "Tiempo de ejecución"
 
@@ -39,7 +40,7 @@ plot "Grafica$name/SalidaTest1$name.txt" using 1:2 with linespoints title "int",
      "Grafica$name/SalidaTest2$name.txt" using 1:2 with linespoints title "float", \
      "Grafica$name/SalidaTest3$name.txt" using 1:2 with linespoints title "double"
 
-# Guardar la gráfica en un archivo de imagen
+# Guardar la gráfica en un archivo de imagen, definiendo el formato primero y el nombre del archivo después
 set term png
 set output "./Grafica$name/${name}.png"
 replot
