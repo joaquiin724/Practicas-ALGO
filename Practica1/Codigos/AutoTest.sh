82f4bc884b8bb6e1c41610508b9feffeee0a264b
Joaquín Avilés de la Fuente <joaquinrojo724@gmail.com>
2024-03-06 17:48:54 +0100

    Cambios latex parte Objetivos

--- Practica1/Codigos/AutoTest.sh
+++ Practica1/Codigos/AutoTest.sh
@@ -1,20 +1,22 @@
 # Este archivo es como el autorun pero sirve para ejecutar un solo archivo, en este
 # caso, el archivo especificado en la siguiente variable:
 
-name="mergesort"
+name="burbuja"
 rm *.txt
 rm -r Grafica$name
 
 for ((j=1; j<=3 ; j++)) do
-    for ((i=5000; i<=1250000 ; i+=50000)) do
+    for ((i=5000; i<=50000 ; i+=5000)) do
             printf "$i \t" >> SalidaTest$j$name.txt
             ./$name $i $j >> SalidaTest$j$name.txt            
             printf "\n" >> SalidaTest$j$name.txt
     done
 done
 
+# Crear un directorio para guardar las gráficas
 mkdir Grafica$name
 
+# Crear un script de Gnuplot para generar la gráfica
 cat << EOF > script_gnuplot.gp
 # Configuración del gráfico
 set xlabel "Tamaño de entrada"
@@ -26,8 +28,13 @@ EOF
 cat << EOF >> script_gnuplot.gp
 
 # Algoritmo $name
-
+# Da un nombre a la gráfica
 set title "Algoritmo - $name"
+# Establece la leyenda, donde 
+    # primer "" indica el lugar de donde tomar los datos
+    # 1:2 indica que se tomará la primera columna para el eje x y la segunda para el eje y
+    # with linespoints indica que se dibujarán líneas uniendo los puntos
+    # segundo "" indica el nombre que se le dará a la serie
 plot "SalidaTest1$name.txt" using 1:2 with linespoints title "int", \
      "SalidaTest2$name.txt" using 1:2 with linespoints title "float", \
      "SalidaTest3$name.txt" using 1:2 with linespoints title "double"
