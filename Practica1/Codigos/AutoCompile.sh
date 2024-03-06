2860544c60a912c2de55b14a87248f50a37fb1a9
miguevr <miguevrgo@gmail.com>
2024-03-06 18:12:51 +0100

    File Structure

--- Practica1/Codigos/AutoCompile.sh
+++ Practica1/Codigos/AutoCompile.sh
@@ -3,10 +3,10 @@
 rm -r Ejecutables
 mkdir Ejecutables
 gcc -Og seleccion.cpp -o Ejecutables/seleccion -lstdc++
-gcc -Og burbuja.cpp -o burbuja -lstdc++
-gcc -Og insercion.cpp -o insercion -lstdc++
-gcc -Og mergesort.cpp -o mergesort -lstdc++
-gcc -Og quicksort.cpp -o quicksort -lstdc++
-gcc -Og fibonacci.cpp -o fibonacci -lstdc++
-gcc -Og hanoi.cpp -o hanoi -lstdc++
-gcc -Og floyd.cpp -o floyd -lstdc++
+gcc -Og burbuja.cpp -o Ejecutables/burbuja -lstdc++
+gcc -Og insercion.cpp -o Ejecutables/insercion -lstdc++
+gcc -Og mergesort.cpp -o Ejecutables/mergesort -lstdc++
+gcc -Og quicksort.cpp -o Ejecutables/quicksort -lstdc++
+gcc -Og fibonacci.cpp -o Ejecutables/fibonacci -lstdc++
+gcc -Og hanoi.cpp -o Ejecutables/hanoi -lstdc++
+gcc -Og floyd.cpp -o Ejecutables/floyd -lstdc++
