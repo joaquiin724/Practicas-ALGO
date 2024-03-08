2fcf4bea2b6e25a805c6d9f5edfe3456ce1330be
miguevr <miguevrgo@gmail.com>
2024-03-08 15:16:34 +0100

    Mejoras

--- Practica1/Codigos/AutoIndividual.sh
+++ Practica1/Codigos/AutoIndividual.sh
@@ -2,24 +2,24 @@
 # Este archivo es como el autorun pero sirve para ejecutar un solo archivo, en este
 # caso, el archivo especificado en la siguiente variable:
 
-name="quicksort"
+name="burbuja"
 rm -r Grafica$name
 
 # Crear un directorio para guardar las gráficas
 mkdir Grafica$name
 
 for ((j=1; j<=3 ; j++)) do
-    for ((i=50000; i<=1250000 ; i+=50000)) do
-        printf "$i \t" >> Grafica$name/SalidaTest$j$name.txt
-        ./Ejecutables/$name $i $j >> Grafica$name/SalidaTest$j$name.txt            
-        printf "\n" >> Grafica$name/SalidaTest$j$name.txt
+    for ((i=5000; i<=125000 ; i+=5000)) do
+        printf "$i \t" >> Grafica$name/Salida$j$name.txt
+        ./Ejecutables/$name $i $j >> Grafica$name/Salida$j$name.txt            
+        printf "\n" >> Grafica$name/Salida$j$name.txt
     done
 done
 
 for ((i=12308; i<=202308 ; i+=10000)) do
-    printf "$i \t" >> Grafica$name/SalidaTest4$name.txt
-    ./Ejecutables/$name $i 4 >> Grafica$name/SalidaTest4$name.txt            
-    printf "\n" >> Grafica$name/SalidaTest4$name.txt
+    printf "$i \t" >> Grafica$name/Salida4$name.txt
+    ./Ejecutables/$name $i 4 >> Grafica$name/Salida4$name.txt            
+    printf "\n" >> Grafica$name/Salida4$name.txt
 done
 
 # Crear un script de Gnuplot para generar la gráfica, todo lo que hay entre EOF y EOF se escribe en un archivo,
@@ -42,9 +42,9 @@ set title "Algoritmo - $name"
     # 1:2 indica que se tomará la primera columna para el eje x y la segunda para el eje y
     # with linespoints indica que se dibujarán líneas uniendo los puntos
     # segundo "" indica el nombre que se le dará a la serie
-plot "Grafica$name/SalidaTest1$name.txt" using 1:2 with linespoints title "int", \
-     "Grafica$name/SalidaTest2$name.txt" using 1:2 with linespoints title "float", \
-     "Grafica$name/SalidaTest3$name.txt" using 1:2 with linespoints title "double" 
+plot "Grafica$name/Salida1$name.txt" using 1:2 with linespoints title "int", \
+     "Grafica$name/Salida2$name.txt" using 1:2 with linespoints title "float", \
+     "Grafica$name/Salida3$name.txt" using 1:2 with linespoints title "double" 
 
 # Guardar la gráfica en un archivo de imagen, definiendo el formato primero y el nombre del archivo después
 set term png
@@ -78,7 +78,7 @@ set title "Algoritmo - $name"
     # 1:2 indica que se tomará la primera columna para el eje x y la segunda para el eje y
     # with linespoints indica que se dibujarán líneas uniendo los puntos
     # segundo "" indica el nombre que se le dará a la serie
-plot "Grafica$name/SalidaTest4$name.txt" using 1:2 with linespoints title "string"
+plot "Grafica$name/Salida4$name.txt" using 1:2 with linespoints title "string"
 
 # Guardar la gráfica en un archivo de imagen, definiendo el formato primero y el nombre del archivo después
 set term png
