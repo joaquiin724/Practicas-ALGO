72ac8a1fd1ff425952968ce4466f2c22a5f70a41
joaquiin724 <joaquinrojo724@gmail.com>
2024-03-08 11:04:23 +0100

    Cambios autoindividual

--- Practica1/Codigos/AutoIndividual.sh
+++ Practica1/Codigos/AutoIndividual.sh
@@ -8,12 +8,18 @@ rm -r Grafica$name
 # Crear un directorio para guardar las gráficas
 mkdir Grafica$name
 
-for ((j=4; j<=4 ; j++)) do
+for ((j=1; j<=3 ; j++)) do
     for ((i=10000; i<=200000 ; i+=10000)) do
-            printf "$i \t" >> Grafica$name/SalidaTest$j$name.txt
-            ./Ejecutables/$name $i $j >> Grafica$name/SalidaTest$j$name.txt            
-            printf "\n" >> Grafica$name/SalidaTest$j$name.txt
+        printf "$i \t" >> Grafica$name/SalidaTest$j$name.txt
+        ./Ejecutables/$name $i $j >> Grafica$name/SalidaTest$j$name.txt            
+        printf "\n" >> Grafica$name/SalidaTest$j$name.txt
     done
+done
+
+for ((i=10000; i<=200000 ; i+=10000)) do
+    printf "$i \t" >> Grafica$name/SalidaTest4$name.txt
+    ./Ejecutables/$name $i 4 >> Grafica$name/SalidaTest$j$name.txt            
+    printf "\n" >> Grafica$name/SalidaTest4$name.txt
 done
 
 # Crear un script de Gnuplot para generar la gráfica, todo lo que hay entre EOF y EOF se escribe en un archivo,
@@ -38,7 +44,8 @@ set title "Algoritmo - $name"
     # segundo "" indica el nombre que se le dará a la serie
 plot "Grafica$name/SalidaTest1$name.txt" using 1:2 with linespoints title "int", \
      "Grafica$name/SalidaTest2$name.txt" using 1:2 with linespoints title "float", \
-     "Grafica$name/SalidaTest3$name.txt" using 1:2 with linespoints title "double"
+     "Grafica$name/SalidaTest3$name.txt" using 1:2 with linespoints title "double" \
+     "Grafica$name/SalidaTest4$name.txt" using 1:2 with linespoints title "string"
 
 # Guardar la gráfica en un archivo de imagen, definiendo el formato primero y el nombre del archivo después
 set term png
