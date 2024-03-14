7563ab69b54aaf1985505eedb396f09afcf13479
joaquiin724 <joaquinrojo724@gmail.com>
2024-03-14 18:04:53 +0100

    Documentación Automedial.py

--- Practica1/Codigos/AutoMedia.py
+++ Practica1/Codigos/AutoMedia.py
@@ -5,12 +5,20 @@ def convert_file(input_file, output_file):
         for line in f_in:
             # Divide la línea en dos partes a partir del caracter ':', el tamaño y los tiempos
             parts = line.split(':') 
-             # Se queda con el tamaño de cada ejecución 
+            # Se queda con el tamaño de cada ejecución. Elimina los espacios en blanco al principio 
+            # y al final con el método strip
             size = parts[0].strip()
-            values = list(map(float, parts[1].split())) # Crea el array de valores de tiempos
+            # Crea el array de valores de tiempos. Convierte los valores de los tiempos a float.
+            # El split separa los valores por el espacio en blanco, es decir, elimina los espacios en blanco
+            # y crea un array con los valores de los tiempos
+            values = list(map(float, parts[1].split()))
             avg = sum(values) / len(values) # Crea el array con los valores de los tiempos promedios
-            avg_str = '{:.10f}'.format(avg) # Formatea el promedio para evitar la notación científica
-            f_out.write(f'{size} {avg_str}\n') # Escribe el tamaño en el fichero y el tiempo promedio de ejecución
+            # Formatea el promedio para evitar la notación científica, convirtiendolo en una cadena
+            # de caracteres con 10 decimales
+            avg_str = '{:.10f}'.format(avg) 
+            # Escribe el tamaño en el fichero y el tiempo promedio de ejecución. Destacar que f indica
+            # que se va a utilizar una cadena de caracteres con formato, y que se pueden incluir variables
+            f_out.write(f'{size} {avg_str}\n') 
 
 # Sirve para poder utilizar los parámetros de entrada
 import sys
@@ -23,7 +31,6 @@ else:
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     convert_file(input_file, output_file)
-
 
 
 
