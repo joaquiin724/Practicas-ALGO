6f6efd45f2e78a79efc6e6c843e9e413fee40def
miguevr <miguevrgo@gmail.com>
2024-03-20 10:58:50 +0100

    Correcion Algoritmo Kadane

--- Practica2/Codigos/SumaMax.cpp
+++ Practica2/Codigos/SumaMax.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <cmath>
+#include <algorithm>
 #include <cstdlib>
 #include <climits>
 #include <cassert>
@@ -40,17 +42,16 @@ data SumaMax (int *v, int inicio, int final){
 }
  
 int kadane(int *a, int size){
-    int max_so_far = INT_MIN, max_ending_here = 0;
- 
+    int max_global = a[0];
+    int max_current = a[0];
+
     for (int i = 0; i < size; i++) {
-        max_ending_here = max_ending_here + a[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
- 
-        if (max_ending_here < 0)
-            max_ending_here = 0;
+        max_current = max(a[i], max_current + a[i]);
+        if (max_current > max_global) {
+            max_global = max_current;
+        }
     }
-    return max_so_far;
+    return max_global;
 }
 
 
