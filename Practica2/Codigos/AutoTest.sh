eb5f350c9d76cd284bc91f259094570a74b2b9b8
joaquiin724 <joaquinrojo724@gmail.com>
2024-04-06 13:13:51 +0200

    Cambio Algoritmo SumaMax con llamada a Kadane y cambio latex

--- Practica2/Codigos/AutoTest.sh
+++ Practica2/Codigos/AutoTest.sh
@@ -8,74 +8,90 @@ cd $script_directory
 chmod +x AutoCompile.sh
 ./AutoCompile.sh
 
+# Cada numero indicará el código que queremos ejecutar segun estan descritos
+# en este archivo en orden descendente, es decir,
+# numero=1 indica SumaMax
+# numero=2 indica Losetas
+# numero=3 indica Viajante
+# numero=4 indica TODOS
+
+n=1
+
 # --------------------------|Ejecución de SumaMax|-----------------------------#
 
-size=5000000
-opcion=3 # [1] Time Test Kadane - Time Test DyV || [2] Correctness Test
-if [ $opcion -eq 1 ]; then
-    rm -f Salidas/SumaMaxKadane.txt
-    rm -f Salidas/SumaMaxDyV.txt
-fi
-if [ $opcion -eq 2 ]; then
-    rm -f Salidas/SumaMax.txt
-fi
-
-
-for ((i=2500; i <= $size; i+=25000)); do
+if(($n == 1 || $n==4)) then
+    size=5000000
+    opcion=2 # [1] Time Test Kadane || [2] Time Test DyV || [3] Correctness Test
     if [ $opcion -eq 1 ]; then
-        printf "$i " >> Salidas/SumaMaxKadane.txt
-        ./Ejecutables/SumaMax $i $opcion >> Salidas/SumaMaxKadane.txt
-        printf "$i " >> Salidas/SumaMaxDyV.txt
-        ./Ejecutables/SumaMax $i $opcion >> Salidas/SumaMaxDyV.txt
+        rm -f Salidas/SumaMaxKadane.txt
     fi
     if [ $opcion -eq 2 ]; then
-        ./Ejecutables/SumaMax $i $opcion >> Salidas/SumaMax.txt
+        rm -f Salidas/SumaMaxDyV.txt
     fi
-done
+    if [ $opcion -eq 3 ]; then
+        rm -f Salidas/SumaMax.txt
+    fi
+
+    for ((i=2500; i <= $size; i+=25000)); do
+        if [ $opcion -eq 1 ]; then
+            printf "$i " >> Salidas/SumaMaxKadane.txt
+            ./Ejecutables/SumaMax $i $opcion >> Salidas/SumaMaxKadane.txt
+        fi
+        if [ $opcion -eq 2 ]; then
+            printf "$i " >> Salidas/SumaMaxDyV.txt
+            ./Ejecutables/SumaMax $i $opcion >> Salidas/SumaMaxDyV.txt
+        fi
+        if [ $opcion -eq 3 ]; then
+            ./Ejecutables/SumaMax $i $opcion >> Salidas/SumaMax.txt
+        fi
+    done
+fi
 
 #---------------------------|Ejecucion Viajante|-------------------------------#
 
-size2=2000 # Para el caso brute force a partir de 12 no se puede ejecutar
-opcion2=3 # [1] Random test || [2] Made test || [3] Get Umbral
-files=(
-    "Paises/Finland.txt" 
-    "Paises/Nicaragua.txt" 
-    "Paises/Djibouti.txt" 
-    "Paises/Egypt.txt" 
-    "Paises/Oman.txt"
-    "Paises/Vietnam.txt"
-    "Paises/Uruguay.txt"
-    "Paises/Canada.txt"
-    "Paises/Greece.txt"
-    "Paises/Japan.txt"
-    "Paises/China.txt"
-    "Paises/Burma.txt"
-    )
-#Solucion: Finland = 520527, Djibouti = 6656, Oman = 86891, Nicaragua = 96132, Egypt = 172387
-# Vietnam = 569288  Uruguay = 79114 Canada = 1290319 Greece = 300899 Japan = 491924 China = 4566563
-# Burma = 959304
+if(($n == 3 || $n==4)) then
 
-if [ $opcion2 -eq 1 ]; then
-    rm -f Salidas/ViajanteRandom.txt
-    
-    for ((i=10; i <= $size2; i+=20)) do
-        ./Ejecutables/Viajante $i $opcion2 >> Salidas/ViajanteRandom.txt
-        # python3 PlotP3.py
-    done
-fi
-if [ $opcion2 -eq 2 ]; then
-    rm -f Salidas/Viajante.txt
-    for ((i=0; i < ${#files[@]}; i++)) do
-        ./Ejecutables/Viajante ${files[i]} $opcion2 >> Salidas/Viajante.txt
-    done
-fi
+    size2=2000 # Para el caso brute force a partir de 12 no se puede ejecutar
+    opcion2=3 # [1] Random test || [2] Made test || [3] Get Umbral
+    files=(
+        "Paises/Finland.txt" 
+        "Paises/Nicaragua.txt" 
+        "Paises/Djibouti.txt" 
+        "Paises/Egypt.txt" 
+        "Paises/Oman.txt"
+        "Paises/Vietnam.txt"
+        "Paises/Uruguay.txt"
+        "Paises/Canada.txt"
+        "Paises/Greece.txt"
+        "Paises/Japan.txt"
+        "Paises/China.txt"
+        "Paises/Burma.txt"
+        )
+    #Solucion: Finland = 520527, Djibouti = 6656, Oman = 86891, Nicaragua = 96132, Egypt = 172387
+    # Vietnam = 569288  Uruguay = 79114 Canada = 1290319 Greece = 300899 Japan = 491924 China = 4566563
+    # Burma = 959304
 
-if [ $opcion2 -eq 3 ]; then
-    for ((i=4; i <= 9; ++i)) do
-        # rm -f Salidas/Viajante$i.txt # Cuidado con borrarlo que tarda 1h en ejecutarse
-        for ((j=0; j < ${#files[@]}; j++)) do
-            ./Ejecutables/Viajante ${files[j]} $opcion2 $i >> Salidas/Viajante$i.txt
+    if [ $opcion2 -eq 1 ]; then
+        rm -f Salidas/ViajanteRandom.txt
+        
+        for ((i=10; i <= $size2; i+=20)) do
+            ./Ejecutables/Viajante $i $opcion2 >> Salidas/ViajanteRandom.txt
+            # python3 PlotP3.py
         done
-    done
-fi
+    fi
+    if [ $opcion2 -eq 2 ]; then
+        rm -f Salidas/Viajante.txt
+        for ((i=0; i < ${#files[@]}; i++)) do
+            ./Ejecutables/Viajante ${files[i]} $opcion2 >> Salidas/Viajante.txt
+        done
+    fi
 
+    if [ $opcion2 -eq 3 ]; then
+        for ((i=4; i <= 9; ++i)) do
+            # rm -f Salidas/Viajante$i.txt # Cuidado con borrarlo que tarda 1h en ejecutarse
+            for ((j=0; j < ${#files[@]}; j++)) do
+                ./Ejecutables/Viajante ${files[j]} $opcion2 $i >> Salidas/Viajante$i.txt
+            done
+        done
+    fi
+fi
--- Practica2/Codigos/SumaMax.cpp
+++ Practica2/Codigos/SumaMax.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <cstring>
 #include <chrono>
+#include <utility>
 using namespace std;
 
 
@@ -18,43 +19,118 @@ struct SumaData{
     int  max_sub;
 };
 
-
-
-// 5 6 -1 1 1 -5 9 1 -5 -2 
-SumaData SumaMax (int *v, int inicio, int final){
-    SumaData result, d1, d2;
-    if (inicio==final){
-        result.max_izq = v[inicio];
-        result.max_dch = v[inicio];
-        result.sum = v[inicio];
-        result.max_sub = v[inicio];
-        return (result);
-    }
-
-    int mid = (final+inicio)/2;
-    (d1)=SumaMax(v, inicio, mid);
-    (d2)=SumaMax(v, mid+1, final);
-    
-    result.max_izq = max(d1.max_izq, d1.sum+d2.max_izq);
-    result.max_dch = max(d2.max_dch, d2.sum+d1.max_dch);
-    result.sum = d1.sum + d2.sum;
-    int max_cross = d1.max_dch + d2.max_izq;
-    result.max_sub = max(max(max_cross, d1.max_sub), d2.max_sub);
-    return (result);
-}
  
-int kadane(int *a, int size){
-    int max_global = a[0];
-    int max_current = a[0];
+/**
+ * Función que recibe un puntero a un vector y su tamaño, y calcula la suma más grande que encuentre
+ * en una subsecuencia del array. La idea es la siguiente, se guardará tanto la suma que se hace a lo largo
+ * del array continuamente como la máxima, es decir, @b max_current y @b max_global . Cuando se avanza en el array 
+ * si la suma de los anteriores junto con el int actual del array es menor que el int actual, se cambiará la suma de 
+ * la subsecuencia actual (por sólo el int) y se avanzará en el array, sumando, si es así el caso, los siguientes números.
+ * 
+ * Destacar que decimos "sí es así el caso" para indicar que si la suma del siguiente int y el nuevo
+ * valor de @b max_current es menor al siguiente se cambiará el valor de dicha variable como hemos comentado
+ * anteriormente.
+ * Es claro que si en algun momento @b max_current supera a @b max_global se acutaliza el valor de @b max_global
+ * 
+ * @param a Puntero a un vector de int
+ * @param size Tamaño del vector
+ * @return Suma máxima de la mejor subsecuencia del vector
+ * 
+*/
+pair<int,int> kadane(int *a, int inicio, int size){
+    int max_global = a[inicio];
+    int max_current = a[inicio];
+    int indice=inicio;
 
-    for (int i = 1; i < size; i++) {
+    for (int i = inicio+1; i < size; i++) {
         max_current = max(a[i], max_current + a[i]);
         if (max_current > max_global) {
             max_global = max_current;
+            indice=i;
         }
     }
-    return max_global;
+
+    pair <int,int> k={max_global, indice};
+    return k;
 }
+
+// 5 6 -1 1 1 -5 9 1 -5 -2 
+// SumaData SumaMax (int *v, int inicio, int final){
+    // SumaData result, d1, d2;
+    // if (inicio==final){
+    //     result.max_izq = v[inicio];
+    //     result.max_dch = v[inicio];
+    //     result.sum = v[inicio];
+    //     result.max_sub = v[inicio];
+    //     return (result);
+    // }
+
+    // int mid = (final+inicio)/2;
+    // d1=SumaMax(v, inicio, mid);
+    // d2=SumaMax(v, mid+1, final);
+    
+    // result.max_izq = max(d1.max_izq, d1.sum+d2.max_izq);
+    // result.max_dch = max(d2.max_dch, d2.sum+d1.max_dch);
+    // result.sum = d1.sum + d2.sum;
+    // int max_cross = d1.max_dch + d2.max_izq;
+    // result.max_sub = max(max(max_cross, d1.max_sub), d2.max_sub);
+    // return (result);
+    // NO SE si lo que tenemos sirve porque hay que poner caso base con llamada a kadane o a n²
+
+    // int caso_base=5;
+
+    // if( (final-inicio+1) <= 5){
+    //     struct Kadane k=kadane(v, final-inicio+1);
+    //     return k;
+    // }
+
+    // int mid=(final+inicio)/2;
+    // struct Kadane k1= SumaMax(v, inicio, mid);
+    // struct Kadane k2=SumaMax(v, mid+1, final);
+
+
+// }
+
+pair<int,int> SumaMax (int *v, int inicio, int final){
+
+    // Destacar que caso_base es el tamaño de la subsecuencia que se considera como base para
+    // aplicar el algoritmo de Kadane. Si aumenta el valor de caso_base, disminuirá el tiempo de ejecución,
+    // por lo que el mejor UMBRAl es aquel que tiene a infinito
+    int caso_base=10000;
+
+    if( (final-inicio) <= caso_base){
+        pair<int,int> k=kadane(v, inicio, final);
+        return k;
+    }
+
+    int mid=(final+inicio)/2;
+    pair<int,int> k1= SumaMax(v, inicio, mid);
+    pair<int,int> k2=SumaMax(v, mid, final);
+
+    int max_global=k1.first;
+    int indice_inicio=k1.second;
+    int indice_final=k2.second;
+    int max_current=k1.first;
+
+    int indiceReturn=indice_inicio;
+    for (int i = indice_inicio+1; i <= indice_final; i++) {
+        max_current = max(v[i], max_current + v[i]);
+        if (max_current > max_global) {
+            max_global = max_current;
+            indiceReturn=i;
+        }
+    }
+
+    max_global=max(max_global, k2.first);
+    if(max_global==k2.first){
+        indiceReturn=k2.second;
+    }
+    pair<int,int> k={max_global, indiceReturn};
+
+    return k;
+}
+
+
 
 int main(int argc, char *argv[]) {
     int TAM= atoi(argv[1]);
@@ -67,14 +143,14 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[2], "1") == 0) { // Time Test Kadane
 
         auto start = chrono::high_resolution_clock::now();
-        kadane(v, TAM);
+        kadane(v,0, TAM);
         auto end = chrono::high_resolution_clock::now();
         std::cout << chrono::duration_cast<chrono::nanoseconds>(end - start).count()<<endl;
 
     } 
     else if (strcmp(argv[2], "2") == 0) { // Time Test DyV
         auto start = chrono::high_resolution_clock::now();
-        SumaMax(v, 0, TAM-1);
+        SumaMax(v, 0, TAM);
         auto end = chrono::high_resolution_clock::now();
         std::cout << chrono::duration_cast<chrono::nanoseconds>(end - start).count()<<endl;
     } 
@@ -83,9 +159,18 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < TAM; i++) {
             kadaneVector[i] = v[i];
         }
+        // {-1,1,1,10,-10,9,2}
+        // int *prueba=new int[7];
+        // prueba[0]=-1;
+        // prueba[1]=1;
+        // prueba[2]=1;
+        // prueba[3]=10;
+        // prueba[4]=-10;
+        // prueba[5]=9;
+        // prueba[6]=2;
 
-        int res1 = SumaMax(v, 0, TAM-1).max_sub;
-        int res2 = kadane(kadaneVector, TAM);
+        int res1 = SumaMax(v, 0, TAM).first;
+        int res2 = kadane(v,0, TAM).first;
         if (res1 != res2) {
             std::cout << "Error [" << TAM << "]: " << res1 << " != " << res2 << endl;
         }
