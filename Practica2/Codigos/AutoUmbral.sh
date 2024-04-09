e7e01405426c3c97ee6b8ab68b030afd1ecf4558
Albertovs03 <aldelavera@gmail.com>
2024-04-09 20:43:46 +0200

    .

--- Practica2/Codigos/AutoUmbral.sh
+++ Practica2/Codigos/AutoUmbral.sh
@@ -55,3 +55,44 @@ gnuplot script_gnuplot.gp
 # Eliminar el script de Gnuplot
 rm script_gnuplot.gp
 done
+
+
+#Calculo umbral empírco según teoria de SumaMax
+
+
+# Crear un script de Gnuplot para generar la gráfica, todo lo que hay entre EOF y EOF se escribe en un archivo,
+# en este caso, script_gnuplot.gp
+cat << EOF > script_gnuplot.gp
+# Configuración del gráfico, se define el nombre de los ejes
+set xlabel "Tamaño de entrada"
+set ylabel "Tiempo de ejecución"
+
+EOF
+# Bucle para generar gráficos para cada algoritmo y tipo de dato
+cat << EOF >> script_gnuplot.gp
+
+# Algoritmo SumaMax
+# Da un nombre a la gráfica
+set title "Algoritmo - SumaMax"
+#Da colores a las lineas
+set style line 1 lt 1 lc rgb "blue"
+set style line 2 lt 1 lc rgb "red"
+# Establece la leyenda, donde 
+    # primer "" indica el lugar de donde tomar los datos
+    # 1:2 indica que se tomará la primera columna para el eje x y la segunda para el eje y
+    # with linespoints indica que se dibujarán líneas uniendo los puntos
+    # segundo "" indica el nombre que se le dará a la serie
+plot "Salidas/SumaMaxKadane.txt" using 1:2 with linespoints linestyle 1 title "Kadane"
+plot "Salidas/SumaMaxDyV.txt" using 1:2 with linespoints linestyle 2 title "DyV"
+
+
+# Guardar la gráfica en un archivo de imagen, definiendo el formato primero y el nombre del archivo después
+set term png
+set output "Graficas/SumaMaxUmbral.png"
+replot
+EOF
+# Ejecutar Gnuplot con el script generado
+gnuplot script_gnuplot.gp
+
+# Eliminar el script de Gnuplot
+rm script_gnuplot.gp
