80fedd95637c13bc4ffe51d5b2bb7f90c7fca028
astralisx <magoru9392@gmail.com>
2024-04-26 10:58:18 +0200

    update P3

---
+++ Practica3/Codigos/P3/Camino.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <limits>
+#include <cstring>
+#include <numeric>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+class Grafo {
+    private:
+    list<list<Coordenada>> 
+}
+
+int main (int argc, char * argv[]){
+
+
+    return 0;
+}
