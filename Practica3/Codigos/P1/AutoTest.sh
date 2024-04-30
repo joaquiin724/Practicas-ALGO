58396b911e4fe3cbc10a1a1256681785218d1e5d
Albertovs03 <aldelavera@gmail.com>
2024-04-30 17:40:52 +0200

    Eficiencias P1

--- Practica3/Codigos/P1/AutoTest.sh
+++ Practica3/Codigos/P1/AutoTest.sh
@@ -3,14 +3,14 @@ script_directory=$(dirname "$0")
 cd $script_directory
 gcc -O2 Herencia.cpp -o Ejecutables/Herencia -lstdc++ -lm
 
-num_bienes=10000
+num_bienes=100000
 
 
 rm -r Salidas/
 mkdir Salidas
 mkdir Salidas/Resultados
 
-for ((i=100; i < $num_bienes; i+=100)); do
+for ((i=1000; i < $num_bienes; i+=1000)); do
 
     # Nombre del archivo de salida
     salida="Salidas/Resultados/Herencia_$i.txt"
