45a4c0649980fa1a21d74291aed24be37f17a67f
miguevrgo <miguevrgo@gmail.com>
2024-05-14 19:39:43 +0200

    Esto es BB para el portatil, porque me voy a casa de mi novia, el codigo fufa pero es basura :

--- Practica4/Codigos/BB/CodigoBase.cpp
+++ Practica4/Codigos/BB/CodigoBase.cpp
@@ -1,31 +1,119 @@
-#include <cmath>
 #include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+//TODO: CODIGO DE MIERDA QUE SE SUPONE QUE FUNCIONA, ATENTAMENTE MIGUE, YA LO MEJORARE, ES PARA TENERLO EN EL PORTATIL
+const int N = 4;
+std::vector<int> final_path(N + 1);
+std::vector<bool> visited(N);
+int final_res = std::numeric_limits<int>::max();
 
-class Point {
-public:
-  constexpr Point(int x, int y) noexcept : x(x), y(y) {}
+void copyToFinal(const std::vector<int>& curr_path) {
+    std::copy(curr_path.begin(), curr_path.end(), final_path.begin());
+    final_path[N] = curr_path[0];
+}
 
-  /**
-   * @brief Calculates the distance between two points using the
-   * Euclidean distance formula.
-   *
-   * @param p other point
-   * @return Distance between the two points
-   */
-  [[nodiscard]] constexpr auto
-  distanceTo(const Point &p) const noexcept -> double {
-    return std::sqrt(std::pow(p.x - x, 2) + std::pow(p.y - y, 2));
-  }
+int firstMin(const std::vector<std::vector<int>>& adj, int i) {
+    int min = std::numeric_limits<int>::max();
+    for (int k = 0; k < N; k++) {
+        if (adj[i][k] < min && i != k) {
+            min = adj[i][k];
+        }
+    }
+    return min;
+}
 
-  [[nodiscard]] constexpr auto getX() const noexcept -> int { return x; }
-  [[nodiscard]] constexpr auto getY() const noexcept -> int { return y; }
+int secondMin(const std::vector<std::vector<int>>& adj, int i) {
+    int first = std::numeric_limits<int>::max(), second = std::numeric_limits<int>::max();
+    for (int j = 0; j < N; j++) {
+        if (i == j) continue;
 
-  friend auto operator<<(std::ostream &os,
-                         const Point &point) -> std::ostream & {
-    os << point.x << ',' << point.y;
-    return os;
-  }
+        if (adj[i][j] <= first) {
+            second = first;
+            first = adj[i][j];
+        } else if (adj[i][j] <= second && adj[i][j] != first) {
+            second = adj[i][j];
+        }
+    }
+    return second;
+}
 
-private:
-  int x, y;
-};
+void TSPRec(const std::vector<std::vector<int>>& adj, int curr_bound, int curr_weight, int level, std::vector<int>& curr_path) {
+    if (level == N) {
+        if (adj[curr_path[level - 1]][curr_path[0]] != 0) {
+            int curr_res = curr_weight + adj[curr_path[level - 1]][curr_path[0]];
+
+            if (curr_res < final_res) {
+                copyToFinal(curr_path);
+                final_res = curr_res;
+            }
+        }
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (adj[curr_path[level - 1]][i] != 0 && !visited[i]) {
+            int temp = curr_bound;
+            curr_weight += adj[curr_path[level - 1]][i];
+
+            if (level == 1) {
+                curr_bound -= ((firstMin(adj, curr_path[level - 1]) + firstMin(adj, i)) / 2);
+            } else {
+                curr_bound -= ((secondMin(adj, curr_path[level - 1]) + firstMin(adj, i)) / 2);
+            }
+
+            if (curr_bound + curr_weight < final_res) {
+                curr_path[level] = i;
+                visited[i] = true;
+
+                TSPRec(adj, curr_bound, curr_weight, level + 1, curr_path);
+            }
+
+            curr_weight -= adj[curr_path[level - 1]][i];
+            curr_bound = temp;
+
+            std::fill(visited.begin(), visited.end(), false);
+            for (int j = 0; j <= level - 1; j++) {
+                visited[curr_path[j]] = true;
+            }
+        }
+    }
+}
+
+void TSP(const std::vector<std::vector<int>>& adj) {
+    std::vector<int> curr_path(N + 1, -1);
+
+    int curr_bound = 0;
+    std::fill(visited.begin(), visited.end(), false);
+
+    for (int i = 0; i < N; i++) {
+        curr_bound += (firstMin(adj, i) + secondMin(adj, i));
+    }
+
+    curr_bound = (curr_bound & 1) ? curr_bound / 2 + 1 : curr_bound / 2;
+
+    visited[0] = true;
+    curr_path[0] = 0;
+
+    TSPRec(adj, curr_bound, 0, 1, curr_path);
+}
+
+int main() {
+    std::vector<std::vector<int>> adj = {
+        {0, 10, 15, 20},
+        {10, 0, 35, 25},
+        {15, 35, 0, 30},
+        {20, 25, 30, 0}
+    };
+
+    TSP(adj);
+
+    std::cout << "Minimum cost: " << final_res << std::endl;
+    std::cout << "Path Taken: ";
+    for (int i = 0; i <= N; i++) {
+        std::cout << final_path[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
