903816d3be88ccec749839ddf967291cf84a515a
joaquiin724 <joaquinrojo724@gmail.com>
2024-05-19 11:19:26 +0200

    Comentarios de las funciones en BB

--- Practica4/Codigos/BB/ViajanteBB.cpp
+++ Practica4/Codigos/BB/ViajanteBB.cpp
@@ -12,149 +12,14 @@
 using namespace std;
 
 /**
- * @brief Class which represents a point in a 2D plane.
- * @note [[nodiscard]] is an attribute that tells the compiler to issue a warning if the return value of a function is ignored.
- * @note constexpr is an attribute that tells the compiler that a function can be evaluated at compile time.
- * @note noexcept is an attribute that tells the compiler that a function does not throw exceptions.
- * @note -> is a new syntax that allows the compiler to deduce the return type of a function.
+ * Función para calcular el mínimo de una matriz de distancias, excluyendo los valores de 0 y 
+ * se pasan como parámetro las filas que se deben ignorar en la búsqueda del mínimo.
  * 
+ * @param matriz matriz de de adyacencia entre los puntos
+ * @param filas_a_ignorar filas que se deben ignorar en la búsqueda del mínimo
  * 
- */
-class Point {
-public:
-    constexpr Point(int x, int y) noexcept : x(x), y(y) {}
-
-    [[nodiscard]] constexpr auto distanceTo(const Point &p) const noexcept -> double {
-        return std::sqrt(std::pow(p.x - x, 2) + std::pow(p.y - y, 2));
-    }
-
-    [[nodiscard]] constexpr auto getX() const noexcept -> int { return x; }
-    [[nodiscard]] constexpr auto getY() const noexcept -> int { return y; }
-
-    bool operator==(const Point &p) const {
-        return x == p.x && y == p.y;
-    }
-
-    friend auto operator<<(std::ostream &os, const Point &point) -> std::ostream & {
-        os << point.x << ',' << point.y;
-        return os;
-    }
-
-private:
-    int x, y;
-};
-
-class Matrix {
-public:
-    Matrix(int size) noexcept : matrix(size, std::vector<double>(size, 0)) {}
-    
-    [[nodiscard]] auto getDistance(int i, int j) const noexcept -> double {
-        return matrix[i][j];
-    }
-
-    void initializeMatrix(const std::vector<Point>& points) {
-        for (int i = 0; i < points.size(); ++i) {
-            for (int j = 0; j < points.size(); ++j) {
-                if (i == j) {
-                    matrix[i][j] = std::numeric_limits<double>::infinity();
-                    continue;
-                }
-                matrix[i][j] = points[i].distanceTo(points[j]);
-            }
-        }
-    }
-private:
-    std::vector<std::vector<double>> matrix;
-};
-
-
-
-
-double totalDistance(const std::vector<Point>& points) {
-    double distance = 0;
-    for (int i = 0; i < points.size() - 1; ++i) {
-        distance += points[i].distanceTo(points[i + 1]);
-    }
-    distance += points.back().distanceTo(points.front());
-    return distance;
-}
-
-std::vector<Point> nearestNeighborTSP(const std::vector<Point>& points) {
-    std::vector<Point> path;
-    std::vector<bool> visited(points.size(), false);
-    path.reserve(points.size());
-    path.emplace_back(points[0]);
-    visited[0] = true;
-
-    for (int i = 0; i < points.size() - 1; ++i) {
-        double minDistance = std::numeric_limits<double>::max();
-        int nearestNeighbor = -1;
-        for (int j = 0; j < points.size(); ++j) {
-            if (!visited[j]) {
-                double distance = path[i].distanceTo(points[j]);
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    nearestNeighbor = j;
-                }
-            }
-        }
-        path.emplace_back(points[nearestNeighbor]);
-        visited[nearestNeighbor] = true;
-    }
-
-    return path;
-}
-
-class BranchBound {
-private:
-    double bestSolution;
-    std::vector<Point> bestPath;
-    std::vector<Point> path;
-public:
-    BranchBound(const std::vector<Point>& points) {
-        path = points;
-        bestPath = path;
-        bestSolution = totalDistance(bestPath);
-    }
-
-    double getBestSolution() {
-        return bestSolution;
-    }
-
-    std::vector<Point> getBestPath() {
-        return bestPath;
-    }
-
-    void getSolution(const std::vector<Point>& currentPath, const double pathDistance) {
-        if (currentPath.size() == path.size()) {
-            double totalDist = pathDistance + currentPath.back().distanceTo(currentPath[0]);
-            if (totalDist < bestSolution) {
-                bestSolution = totalDist;
-                bestPath = currentPath;
-            }
-            return;
-        }
-
-        for (const Point& point : path) {
-            if (std::find(currentPath.begin(), currentPath.end(), point) == currentPath.end()) {
-                double newDistance = pathDistance + currentPath.back().distanceTo(point);
-
-                if (newDistance < bestSolution * currentPath.size() / bestPath.size()) {
-                    if (newDistance < bestSolution * currentPath.size() / bestPath.size()) {
-                        std::cout << "projectedDistance: " << newDistance << std::endl;
-                        std::cout << "bestSolution: " << bestSolution * currentPath.size() / bestPath.size() << std::endl;
-                    }
-                    std::vector<Point> newPath = currentPath;
-                    newPath.emplace_back(point);
-                    getSolution(newPath, newDistance);
-                }
-            }
-        }
-    }
-};
-
-
-
+ * @return suma de los mínimos de cada fila
+*/
 double minimo(const vector<vector<double>>& matriz, const vector<int>& filas_a_ignorar = vector<int>()) {
     double suma_minimos = 0.0;
 
@@ -182,6 +47,14 @@ double minimo(const vector<vector<double>>& matriz, const vector<int>& filas_a_i
     return suma_minimos;
 }
 
+/**
+ * Función para encontrar los números faltantes desde 0 hasta n en un vector de enteros
+ * 
+ * @param vec vector de enteros donde buscar
+ * @param n número máximo a buscar
+ * 
+ * @return vector con los números faltantes
+*/
 vector<int> numeros_faltantes(const vector<int>& vec, int n) {
     vector<bool> presente(n + 2, false); // Vector para marcar los números presentes, de 0 a n
 
@@ -204,12 +77,28 @@ vector<int> numeros_faltantes(const vector<int>& vec, int n) {
 }
 
 
+/**
+ * @brief Estructura para almacenar los datos de un nodo en el algoritmo de Branch and Bound
+*/
 struct Nodo {
+    /**
+     * @brief Miembros de la estructura Nodo
+    */
     vector<int> path;
+
+    /**
+     * @brief Distancia recorrida hasta el nodo actual
+    */
     double distancia_recorrida;
+
+    /**
+     * @brief Cota inferior para el nodo actual
+    */
     double cota_inferior;
 
-    // Constructor para inicializar los miembros del struct
+    /**
+     * @brief Constructor de la estructura Nodo
+    */
     Nodo(vector<int>&inicial, double dist_rec, double cota_inf) {
         path = inicial;
         distancia_recorrida = dist_rec;
@@ -217,13 +106,35 @@ struct Nodo {
     }
 };
 
-// Comparador personalizado
+/**
+ * Función comparador de nodos para la cola de prioridad en el algoritmo de Branch and Bound.
+*/
 struct Comparador {
     bool operator()(const Nodo& a, const Nodo& b) {
         return a.distancia_recorrida > b.distancia_recorrida; // Min-Heap basado en el costo actual
     }
 };
 
+/**
+ * Función para resolver el problema del viajante de comercio utilizando el algoritmo de Branch and Bound.
+ * La idea es la siguiente:
+ *  - Se crea un nodo con el punto inicial y se agrega a la cola de prioridad.
+ *  - Mientras la cola no esté vacía:
+ *     - Se extrae el nodo con menor costo actual (a partir de la cota inferior y haciendo uso del comparador
+ *      definido anteriormente).
+ *    - Si el camino actual tiene todos los puntos menos unos, sabemos de forma determinada cual queda
+ *      mediante la función faltantes de antes, por lo que se calcula la distancia a dicho punto y al inicial
+ *     y se agrega a la solución si es mejor que la actual.
+ *   - Si no, se generan los nodos hijos con los puntos faltantes y se agregan a la cola de prioridad.
+ * Destacar que esta idea es un poco diferente a la presentada en las diapositivas, pero la eficiencia
+ * es la misma.
+ * 
+ * @param points vector con los puntos a visitar
+ * @param distancias matriz de distancias entre los puntos
+ * @param inicial punto inicial
+ * 
+ * @return vector con el camino que minimiza la distancia total
+*/
 vector<int> Branch_and_Bound (vector<int>& points, vector< vector<double>> &distancias, int inicial){
     priority_queue<Nodo, vector<Nodo>, Comparador> no_visitados;
     vector<int> mejor_camino= {inicial};
@@ -237,15 +148,6 @@ vector<int> Branch_and_Bound (vector<int>& points, vector< vector<double>> &dist
         actual = no_visitados.top();
         no_visitados.pop();
 
-        // // Mostrar el camino actual
-        // for (int i = 0; i < actual.path.size(); ++i) {
-        //     cout << actual.path[i] << " ";
-        // }
-        // cout << endl;
-
-        // // Mostrar cota inferior
-        // cout << "Cota inferior: " << actual.cota_inferior << endl;
-
         if (actual.path.size() == points.size()-1) {
             vector<int> faltantes= numeros_faltantes(actual.path, points.size()-1);
             actual.distancia_recorrida += distancias[actual.path.back()][faltantes[0]];
@@ -253,32 +155,18 @@ vector<int> Branch_and_Bound (vector<int>& points, vector< vector<double>> &dist
             actual.path.push_back(faltantes[0]);
             actual.path.push_back(inicial);
             if (actual.distancia_recorrida < costo_minimo) {
-                // // Mostrar el camino actual
-                // cout << "------------------------------------\n"<< endl;
-                // for (int i = 0; i < actual.path.size(); ++i) {
-                //     cout << actual.path[i] << " ";
-                // }
-                // cout << endl;
-                // cout << "------------------------------------\n"<< endl;
                 costo_minimo = actual.distancia_recorrida;
                 mejor_camino = actual.path;
             }
         } 
         else { 
-            if(actual.cota_inferior < costo_minimo){
+            if(actual.cota_inferior < costo_minimo ){
                 vector<int> faltantes = numeros_faltantes(actual.path, points.size()-1);
                 for (int i = 0; i < faltantes.size(); ++i) {
                     Nodo nuevo= actual;
                     nuevo.path.push_back(faltantes[i]);
                     nuevo.distancia_recorrida= nuevo.distancia_recorrida+ distancias[actual.path.back()][faltantes[i]];
                     nuevo.cota_inferior = nuevo.distancia_recorrida + minimo(distancias, nuevo.path);
-                    // // MOstrar el path de nuevo
-                    // for (int i = 0; i < nuevo.path.size(); ++i) {
-                    //     cout << nuevo.path[i] << " ";
-                    // }
-                    // cout << endl;
-                    // // Mostrar la suma para cota_inferior
-                    // cout << "Cota inferior: " << nuevo.cota_inferior << "=" << nuevo.distancia_recorrida << "(" << actual.distancia_recorrida<<"+"<<distancias[actual.path.back()][faltantes[i]]<<")+" << minimo(distancias,nuevo.path) << endl;
                     no_visitados.push(nuevo);
                 }
             }
@@ -289,7 +177,72 @@ vector<int> Branch_and_Bound (vector<int>& points, vector< vector<double>> &dist
 
 }
 
-// Función para calcular la distancia total del camino
+/**
+ * Función para resolver el problema del viajante de comercio utilizando el algoritmo de Branch and Bound.
+ * La idea es la siguiente:
+ *  - Se crea un nodo con el punto inicial y se agrega a la cola de prioridad.
+ *  - Mientras la cola no esté vacía:
+ *   - Se extrae el nodo con menor costo actual (a partir de la cota inferior y haciendo uso del comparador
+ *     definido anteriormente).
+ *   - Si el camino actual tiene todos los puntos añadimos el inicial para tener el camino completo y
+ *    calculamos la distancia total, si es mejor que la actual, actualizamos la solución.
+ *   - Si no, se generan los nodos hijos con los puntos faltantes y se agregan a la cola de prioridad.
+ * Versión 2, con un mayor parecido al código presentado en las diapositivas, pero misma eficiencia.
+ * 
+ * @param points vector con los puntos a visitar
+ * @param distancias matriz de distancias entre los puntos
+ * @param inicial punto inicial
+ * 
+ * @return vector con el camino que minimiza la distancia total
+*/
+vector<int> Branch_and_Bound2 (vector<int>& points, vector< vector<double>> &distancias, int inicial){
+    priority_queue<Nodo, vector<Nodo>, Comparador> no_visitados;
+    vector<int> mejor_camino= {inicial};
+    Nodo actual( mejor_camino, 0, minimo(distancias));
+    no_visitados.push(actual);
+
+    double costo_minimo = numeric_limits<double>::infinity();
+    mejor_camino.clear();
+
+    while (!no_visitados.empty()) {
+        actual = no_visitados.top();
+        no_visitados.pop();
+
+        if(actual.cota_inferior < costo_minimo ){
+            vector<int> faltantes = numeros_faltantes(actual.path, points.size()-1);
+            for (int i = 0; i < faltantes.size(); ++i) {
+                Nodo nuevo= actual;
+                nuevo.path.push_back(faltantes[i]);
+                nuevo.distancia_recorrida= nuevo.distancia_recorrida+ distancias[actual.path.back()][faltantes[i]];
+                nuevo.cota_inferior = nuevo.distancia_recorrida + minimo(distancias, nuevo.path);
+                
+                if (nuevo.path.size() == points.size()) {
+                    nuevo.distancia_recorrida += distancias[nuevo.path.back()][inicial];
+                    nuevo.path.push_back(inicial);
+                    if (nuevo.distancia_recorrida < costo_minimo) {
+                        costo_minimo = nuevo.distancia_recorrida;
+                        mejor_camino = nuevo.path;
+                    }
+                } 
+                else{
+                    no_visitados.push(nuevo);
+                }
+            }
+        }
+    }
+
+    return mejor_camino;
+
+}
+
+/**
+ * @brief Función para calcular la distancia total de un camino dado
+ * 
+ * @param distancias matriz de distancias entre los puntos
+ * @param camino vector con el orden de los nodos a visitar
+ * 
+ * @return distancia total del camino
+*/
 double calcularDistanciaTotal(const vector<vector<double>>& distancias, const vector<int>& camino) {
     double total = 0.0;
     for (int i = 0; i < camino.size() - 1; ++i) {
@@ -311,22 +264,9 @@ double calcularDistanciaTotal(const vector<vector<double>>& distancias, const ve
  * @note iota generates a sequence of numbers from 0 to n.
  * @note If needed, it can return the permutation that generates the minimum distance.
  * 
- * @param points which will be used to calculate the minimum distance
- * @return minimum distance to visit all the points
- */
-
-/**
- * @brief This function calculates the minimum distance required to visit
- * all the points in the vector once, to do so, it generates all the possible
- * permutations of the vector and calculates the distance needed to visit all
- * the points in the order of the permutation.
- * 
- * @note Distance from first to last point has to be calculated to make a closed path.
- * @note iota generates a sequence of numbers from 0 to n.
- * @note If needed, it can return the permutation that generates the minimum distance.
- * 
- * @param points which will be used to calculate the minimum distance
- * @return minimum distance to visit all the points
+ * @param adjacencyMatrix matriz con la distancia entre los puntos
+ * @param startNode nodo inicial
+ * @return std::vector<int> vector con el orden de los nodos que minimiza la distancia
  */
 std::vector<int> bruteForceTSP(const std::vector<std::vector<double>>& adjacencyMatrix, int startNode) {
     int numNodes = adjacencyMatrix.size();
@@ -361,7 +301,14 @@ std::vector<int> bruteForceTSP(const std::vector<std::vector<double>>& adjacency
     return bestPath;
 }
 
-// Función para leer una matriz desde un archivo
+
+/**
+ * @brief Función para leer una matriz de adyacencia desde un archivo de texto.
+ * 
+ * @param nombreArchivo nombre del archivo a leer
+ * 
+ * @return matriz de adyacencia leída desde el archivo
+*/
 vector<vector<double>> leerMatrizDesdeArchivo(const string& nombreArchivo) {
     ifstream archivo(nombreArchivo);
     vector<vector<double>> matriz;
@@ -408,15 +355,6 @@ int main(int argc, char* argv[]) {
     //     std::cerr << "Usage: " << argv[0] << " <size or filename> <mode>" << std::endl;
     //     return 1;
     // }
-
-    // Definir una matriz 5x5
-    // vector<vector<double>> matriz = {
-    //     {0, 14, 4, 11, 18},
-    //     {14, 0, 5, 7, 7},
-    //     {4, 5, 0, 9, 17},
-    //     {11, 7, 9, 0, 4},
-    //     {18, 7, 17, 4, 0}
-    // };
 
     vector<int> solucion;
     vector<int> puntos;
